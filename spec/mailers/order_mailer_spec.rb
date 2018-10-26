453f7b47cf697db3e294b7e960144818ed4e0218
Mayowa Pitan <mayowa.pitan@andela.com>
2018-10-26 08:12:28 +0100

    add mailer specs

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
+require "support/mailer"
 
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#order_confirmation" do
+    let(:mailer_view) { 'order_confirmation' }
+    let(:mailer_subject) { 'Order Confirmation' }
+    let(:mailer_body) { 'You have successfully ordered' }
+
+    it_behaves_like 'A well-working mailer'
+  end
+
+  describe "#order_status_update" do
+    let(:mailer_view) { 'order_status_update' }
+    let(:mailer_subject) { 'The status of your order has been updated' }
+    let(:mailer_body) { 'the status of your order has been updated' }
+
+    it_behaves_like 'A well-working mailer'
+  end
 end
